00e4e24e0a56925731be487080f03c4da94837ad
ChuckNezi <jnezia1@gmail.com>
2020-04-11 16:43:29 -0400

    added the ability to edit and delete

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, SimpleChanges, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +6,57 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements 
+OnInit,
+OnChanges,
+DoCheck,
+AfterContentInit,
+AfterContentChecked,
+AfterViewInit,
+AfterViewChecked,
+OnDestroy
+{
   @Input('srvElement') element: {type:string, name:string, content:string};
+  @Input() name: string;
 
-  constructor() { }
+  constructor() { 
+    console.log('constructor')
+  }
+  
+
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges called!')
+    console.log(changes)
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit Called');
+  }
+
+  ngDoCheck(){
+    console.log('do check')
+  }
+
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit called!')
+  }
+
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked called!')
+  }
+
+  ngAfterViewInit(){
+    console.log('ngAfterContentInit called!')
+  }
+
+
+  ngAfterViewChecked(){
+    console.log('ngAfterContentChecked called!')
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy called');
   }
 
 }
